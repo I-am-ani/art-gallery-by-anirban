ea43b839b11b0fda1201b83300ade67001c37513
Anirban <anirban.konar@logichive.in>
2024-02-21 19:29:47 +0530

    fix: image issue

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -18,22 +18,22 @@ import lgShare from 'lightgallery/plugins/share';
 import lgRotate from 'lightgallery/plugins/rotate';
 
 const images = [
-    { src: "/1.jpg", alt: "1" },
-    { src: "/2.jpg", alt: "2" },
-    { src: "/3.jpg", alt: "3" },
-    { src: "/4.jpg", alt: "4", },
-    { src: "/5.jpg", alt: "5", },
-    { src: "/6.jpg", alt: "6", },
-    { src: "/7.jpg", alt: "7", },
-    { src: "/8.jpg", alt: "8", },
-    { src: "/1.jpg", alt: "9" },
-    { src: "/2.jpg", alt: "10" },
-    { src: "/3.jpg", alt: "11" },
-    { src: "/4.jpg", alt: "12", },
-    { src: "/5.jpg", alt: "13", },
-    { src: "/6.jpg", alt: "14", },
-    { src: "/7.jpg", alt: "15", },
-    { src: "/8.jpg", alt: "16", },
+    { src: "./1.jpg", alt: "1" },
+    { src: "./2.jpg", alt: "2" },
+    { src: "./3.jpg", alt: "3" },
+    { src: "./4.jpg", alt: "4", },
+    { src: "./5.jpg", alt: "5", },
+    { src: "./6.jpg", alt: "6", },
+    { src: "./7.jpg", alt: "7", },
+    { src: "./8.jpg", alt: "8", },
+    { src: "./1.jpg", alt: "9" },
+    { src: "./2.jpg", alt: "10" },
+    { src: "./3.jpg", alt: "11" },
+    { src: "./4.jpg", alt: "12", },
+    { src: "./5.jpg", alt: "13", },
+    { src: "./6.jpg", alt: "14", },
+    { src: "./7.jpg", alt: "15", },
+    { src: "./8.jpg", alt: "16", },
 ]
 
 export function Gallery() {
